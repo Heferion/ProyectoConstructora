1f3917fe7f1742b845e4523f0c8ca023ff83e215
Jeffrey Loaiza <jeffreyloaiza92@hotmail.com>
2021-04-08 10:03:56 -0500

    Solucion de union de versiones

--- BackendConstructora/src/controllers/index.ts
+++ BackendConstructora/src/controllers/index.ts
@@ -1,29 +1,27 @@
-export * from './ping.controller';
-export * from './ciudad-pais.controller';
-export * from './pais-ciudad.controller';
-export * from './proyecto-ciudad.controller';
-export * from './ciudad-proyecto.controller';
-export * from './bloque-proyecto.controller';
-export * from './proyecto-bloque.controller';
-export * from './inmueble-bloque.controller';
 export * from './bloque-inmueble.controller';
-export * from './solicitud-inmueble.controller';
-export * from './inmueble-solicitud.controller';
-export * from './registro-pago-solicitud.controller';
-export * from './solicitud-registro-pago.controller';
-export * from './solicitud-cliente.controller';
-export * from './cliente-solicitud.controller';
-export * from './informacion-financiera-cliente.controller';
-<<<<<<< HEAD
-export * from './usuario.controller';
-=======
+export * from './bloque-proyecto.controller';
 export * from './bloque.controller';
+export * from './ciudad-pais.controller';
+export * from './ciudad-proyecto.controller';
 export * from './ciudad.controller';
+export * from './cliente-solicitud.controller';
 export * from './cliente.controller';
+export * from './informacion-financiera-cliente.controller';
 export * from './informacion-financiera.controller';
+export * from './inmueble-bloque.controller';
+export * from './inmueble-solicitud.controller';
 export * from './inmueble.controller';
+export * from './pais-ciudad.controller';
 export * from './pais.controller';
+export * from './ping.controller';
+export * from './proyecto-bloque.controller';
+export * from './proyecto-ciudad.controller';
 export * from './proyecto.controller';
+export * from './registro-pago-solicitud.controller';
 export * from './registro-pago.controller';
+export * from './solicitud-cliente.controller';
+export * from './solicitud-inmueble.controller';
+export * from './solicitud-registro-pago.controller';
 export * from './solicitud.controller';
->>>>>>> ea77b74ef7fcfd9a24d42a0be1fa808a3fe1e8c1
+export * from './usuario.controller';
+
