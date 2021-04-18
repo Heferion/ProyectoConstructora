158cc436003e267e2645dbba9c62d559c71f90d5
scardona55 <66040387+scardona55@users.noreply.github.com>
2021-04-18 16:27:12 -0500

    Pagina principal y enrutamiento

--- FrontedConstructora/src/app/app-routing.module.ts
+++ FrontedConstructora/src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404Component } from './publico/errores/error404/error404.component';
+import { InicioComponent } from './publico/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'inicio',
+    component:InicioComponent
+  },
+  {
+    path:'',
+    pathMatch: 'full',
+    redirectTo: '/inicio',
+  },
+  {
+    path:'seguridad',
+    loadChildren: ()=> import('./modulos/seguridad/seguridad.module').then(m => m.SeguridadModule)
+  },
+  {
+    path:'usuario',
+    loadChildren: ()=> import('./modulos/usuario/usuario.module').then(m => m.UsuarioModule)
+  },
+  {
+    path:'venta',
+    loadChildren: ()=> import('./modulos/venta/venta.module').then(m => m.VentaModule)
+  },
+  {
+    path:'reporte',
+    loadChildren: ()=> import('./modulos/reporte/reporte.module').then(m => m.ReporteModule)
+  },
+  {
+    path:'parametrizacion',
+    loadChildren: ()=> import('./modulos/parametrizacion/parametrizacion.module').then(m => m.ParametrizacionModule)
+  },
+  {
+    path:'**',
+    component:Error404Component
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
