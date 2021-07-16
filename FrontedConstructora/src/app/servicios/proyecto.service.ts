6ec03904f06b33832056f520ec942ed8e2605de8
mcgp <maria.1701614679@ucaldas.edu.co>
2021-07-16 04:39:02 -0500

    Creacion de listar-proyecto

--- FrontedConstructora/src/app/servicios/proyecto.service.ts
+++ FrontedConstructora/src/app/servicios/proyecto.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { DatosGenerales } from '../config/datos.generales';
 import { ProyectoModelo } from '../modelos/proyecto.modelo';
+import { SeguridadService } from './seguridad.service';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +12,57 @@ import { ProyectoModelo } from '../modelos/proyecto.modelo';
 export class ProyectoService {
 
   url: String = DatosGenerales.url;
+  token?: String ="";
 
-  constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient,
+    private servicioSeguridad: SeguridadService) {
+      this.token = this.servicioSeguridad.ObtenerToken(); 
+  }
+
+  ListarRegistros(): Observable<ProyectoModelo[]> {
+    return this.http.get<ProyectoModelo[]>(`${this.url}/proyecto`);
+  }
+
+
+  BuscarRegistro(id: number): Observable<ProyectoModelo> {
+    return this.http.get<ProyectoModelo>(`${this.url}/proyecto/${id}`);
+  }
+
+  AlmacenarRegistro(modelo: ProyectoModelo): Observable<ProyectoModelo> {
+    return this.http.post<ProyectoModelo>(
+      `${this.url}/proyecto`,
+      {
+        nombre: modelo.nombre
+      },
+      {
+        headers: new HttpHeaders({
+          "Authorization":`Bearer ${this.token}`
+        })
+      });
+  }
+
+
+  ModificarRegistro(modelo: ProyectoModelo): Observable<ProyectoModelo> {
+    return this.http.put<ProyectoModelo>(
+      `${this.url}/proyecto/${modelo.id}`,
+      {
+        nombre: modelo.nombre
+      },
+      {
+        headers: new HttpHeaders({
+          "Authorization":`Bearer ${this.token}`
+        })
+      });
+  }
+
+  EliminarRegistro(id: number): Observable<ProyectoModelo> {
+    return this.http.delete<ProyectoModelo>(
+      `${this.url}/proyecto/${id}`,
+      {
+        headers: new HttpHeaders({
+          "Authorization":`Bearer ${this.token}`
+        })
+      });
   }
 
 }
